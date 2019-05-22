2b659d379407b9ec6b4e8680227e6ed69e99a212
menavneet <menavneet@github.com>
2019-05-22 20:58:40 +0530

    wed 20:58

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/department',(req,res,next)=>{
+  res.render('./department/index')
+});
+
+
+
 router.get('/admin',(req,res,next)=>{
   res.render('./admin/index');
 });
+
+router.get('/admin/newApplication',(req,res,next)=>{
+  res.render('./admin/newApplicationForm');
+})
 
 module.exports = router;
