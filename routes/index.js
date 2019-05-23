5d78c787267b43f90257eb9fb324f122663af1e6
menavneet <menavneet@github.com>
2019-05-23 12:45:13 +0530

    Thu 12:45

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,22 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/department',(req,res,next)=>{
-  res.render('./department/index')
+
+  dummyData=[
+    {_id:1,name:'Application 1',description:'Application 1 Description With Long Text ',created_at:Date.now(),created_by:'Dummy User',last_date_to_response:Date.now()},
+    {_id:2,name:'Application 1',description:'Application 1 Description With Long Text ',created_at:Date.now(),created_by:'Dummy User',last_date_to_response:Date.now()},
+    {_id:3,name:'Application 1',description:'Application 1 Description With Long Text ',created_at:Date.now(),created_by:'Dummy User',last_date_to_response:Date.now()},
+    {_id:4,name:'Application 1',description:'Application 1 Description With Long Text ',created_at:Date.now(),created_by:'Dummy User',last_date_to_response:Date.now()}
+  ]
+  res.render('./department/index',{applications:dummyData,name:'Department Name'})
 });
 
-
+router.get('/department/application',(req,res,next)=>{
+  var application={}
+  application._id=req.query.id
+  console.log('-------')
+  res.render('./department/application',{application:application})
+})
 
 router.get('/admin',(req,res,next)=>{
   res.render('./admin/index');
